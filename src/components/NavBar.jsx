ff05233319dbc4ac1b88e4e7e8c32e74ca2202b5
laupadron <laupadron1458@gmail.com>
2022-11-28 20:50:40 -0300

    error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import  Link  from 'react-router-dom';
 
 const NavBar = () => {
  return (
-  <div className='navBar'>
+  <div className='navBar '>
     
       <Navbar bg="light" variant="light" className='general-nav'>
         <Container className='container-nav'>
